4b2987323b65f37831764fd7b07b8af535f918ab
ScottishCrafter_ <scottishcrafter@outlook.com>
2024-08-23 16:03:45 +0100

    Added First Item

--- src/main/java/uk/slfhstd/scottyslu/ScottysLondonUnderground.java
+++ src/main/java/uk/slfhstd/scottyslu/ScottysLondonUnderground.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import uk.slfhstd.scottyslu.item.ModItems;
 
 public class ScottysLondonUnderground implements ModInitializer {
 	public static final String MOD_ID = "scottylu";
@@ -12,6 +13,7 @@ public class ScottysLondonUnderground implements ModInitializer {
 	@Override
 	public void onInitialize() {
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+		LOGGER.info("Hello from Scottys London Underground");
 	}
 }
---
+++ src/main/java/uk/slfhstd/scottyslu/item/ModItems.java
@@ -0,0 +1,29 @@
+package uk.slfhstd.scottyslu.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import uk.slfhstd.scottyslu.ScottysLondonUnderground;
+
+public class ModItems {
+
+    public static final Item OYSTER = registerItem("oyster", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngredientTab(FabricItemGroupEntries entries) {
+        entries.add(OYSTER);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(ScottysLondonUnderground.MOD_ID, name), item);
+    }
+    public static void registerModItems() {
+        ScottysLondonUnderground.LOGGER.info("Registering Items for " + ScottysLondonUnderground.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(ModItems::addItemsToIngredientTab);
+    }
+}
